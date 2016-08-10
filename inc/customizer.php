897de52a0f4cd137d676b2a326d60f207a228a3e
Fabian Stiehle <f.stiehle@gmail.com>
2016-08-10 11:57:17 +0200

    customizer sanitization

--- inc/customizer.php
+++ inc/customizer.php
@@ -18,12 +18,13 @@ function constanzia_customize_register($wp_customize) {
     /* new section */
     $wp_customize->add_section('constanzia_footer', array(
         'title' => __('Footer', 'constanzia'),
-        'priority' => 999
+        'priority' => 999,
     ));
     
     /* footer text */
     $wp_customize->add_setting('footer_text', array(
-        'default' => 'Constanzia Theme designed by Anbian and powered by WordPress'
+        'default' => 'Constanzia Theme powered by WordPress',
+        'sanitize_callback' => 'sanitize_text_field'
     ));
     
     $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'footer_text', array(
@@ -51,7 +52,8 @@ function constanzia_customize_register($wp_customize) {
         $wp_customize->add_setting($color['slug'], array(
             'default' => $color['default'],
             'type' => 'option',
-            'capability' => 'edit_theme_options'
+            'capability' => 'edit_theme_options',
+            'sanitize_callback' => 'sanitize_hex_color'
         ));
         // CONTROLS
         $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, $color['slug'], array(
@@ -67,19 +69,20 @@ function constanzia_customize_register($wp_customize) {
     ));
     
     $wp_customize->add_setting('main_avatar', array(
-        'transport' => 'postMessage'
+        'transport' => 'postMessage',
+        'sanitize_callback' => 'constanzia_sanitize_image'
     ));
     
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'main_avatar', array(
         'section' => 'global_options',
         'label' => 'Main Avatar'
-    )));
-    
+    )));    
     
     $wp_customize->add_setting('sidebar_alignment', array(
         'default' => true,
         'transport' => 'postMessage',
-        'default' => 'right'
+        'default' => 'right',
+        'sanitize_callback' => 'constanzia_sanitize_select'
     ));
     
     $wp_customize->add_control('sidebar_alignment', array(
@@ -93,6 +96,52 @@ function constanzia_customize_register($wp_customize) {
     ));
 }
 add_action('customize_register', 'constanzia_customize_register');
+
+/**
+ * Sanitization: select  
+ * Control: select, radio 
+ * 
+ * Sanitization callback for 'select' and 'radio' type controls. 
+ * This callback sanitizes $input as a slug, and then validates
+ * $input against the choices defined for the control.
+ * Source: https://github.com/WPTRT/code-examples/blob/master/customizer/sanitization-callbacks.php
+ */
+function constanzia_sanitize_select($input, $setting) {	
+	
+    // Ensure input is a slug
+	$input = sanitize_key($input);
+	
+	// Get list of choices from the control
+	// associated with the setting
+	$choices = $setting->manager->get_control($setting->id)->choices;
+	
+	// If the input is a valid key, return it;
+	// otherwise, return the default
+	if (array_key_exists($input, $choices)) {
+        return $input;
+    } else {
+        return $setting->default;
+    }
+}
+     
+/**
+ * Sanitizes input for valid filename and image mime_type
+ * From: https://shellcreeper.com/how-to-sanitize-image-upload/
+ */
+function constanzia_sanitize_image($input) {
+    $output = '';
+ 
+    // check file type 
+    // https://codex.wordpress.org/Function_Reference/wp_check_filetype
+    $filetype = wp_check_filetype($input);
+    $mime_type = $filetype['type'];
+ 
+    // only mime type "image" allowed
+    if (strpos($mime_type, 'image') !== false) {
+        $output = $input;
+    }
+    return $output;
+}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
